2306a0c7e23deccbac5d764bf18177a164607b96
Henrik Skog <henrikskog01@gmail.com>
2022-05-24 18:24:48 +0200

    add transition to background
    Co-authored-by: jorgensandhaug <jorgensandhaug@users.noreply.github.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,13 @@
 import Head from 'next/head'
 import type { NextPage } from 'next'
 import { useTime } from 'react-timer-hook'
-import useSound from 'use-sound';
+import useSound from 'use-sound'
 
 let previousWork: boolean | null = null
 
 const Home: NextPage = () => {
   const { minutes, seconds } = useTime({ format: '12-hour' })
-  const [play] = useSound('/sounds/pling.mp3');
-
-
+  const [play] = useSound('/sounds/pling.mp3')
 
   const work = minutes % 30 < 25
   const minusOne = seconds == 0 ? 0 : 1
@@ -31,9 +29,11 @@ const Home: NextPage = () => {
         background: work ? 'rgb(66,70,169)' : '#EAFF7B',
         color: work ? '#F3ECFF' : '#595959',
       }}>
-        <Head>
-          <title>{work ? '🏋️': '🏖'} {displayMin}:{displaySeconds}</title>
-        </Head>
+      <Head>
+        <title>
+          {work ? '🏋️' : '🏖'} {displayMin}:{displaySeconds}
+        </title>
+      </Head>
       <div className="content">
         <p className="time">
           {displayMin}:{displaySeconds}
